f8111716eca6485e16ade456201df9f20a82ba2a
Luciuzs <ainis32133@gmail.com>
2025-12-04 00:30:06 +0200

    Added css and js libraries, translations

--- resources/lang/lt/messages.php
+++ resources/lang/lt/messages.php
@@ -1,1 +1,13 @@
 <?php
+return [
+    'conferences' => 'Konferencijos',
+    'title' => 'Pavadinimas',
+    'description' => 'Aprašymas',
+    'date' => 'Data',
+    'address' => 'Adresas',
+    'create' => 'Sukurti',
+    'edit' => 'Redaguoti',
+    'delete' => 'Ištrinti',
+    'login' => 'Prisijungti',
+    'logout' => 'Atsijungti',
+];
