7f15a220b50299157163046800d24b831e2e49ff
Luciuzs <ainis32133@gmail.com>
2025-12-04 00:04:29 +0200

    Add controllers, requests and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ConferenceController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// auth routes
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Public
+Route::get('/conferences', [ConferenceController::class, 'index'])->name('conferences.index');
+Route::get('/conferences/{id}', [ConferenceController::class, 'show'])->name('conferences.show');
+
+// Protected
+Route::middleware('auth')->group(function () {
+    Route::get('/conferences/create', [ConferenceController::class, 'create'])->name('conferences.create');
+    Route::post('/conferences', [ConferenceController::class, 'store'])->name('conferences.store');
+    Route::get('/conferences/{id}/edit', [ConferenceController::class, 'edit'])->name('conferences.edit');
+    Route::put('/conferences/{id}', [ConferenceController::class, 'update'])->name('conferences.update');
+    Route::delete('/conferences/{id}', [ConferenceController::class, 'destroy'])->name('conferences.destroy');
 });
